68d7bf427c284eef89bfd456b973941659813b94
Scott <swartho9@msu.edu>
2016-03-19 18:55:34 -0400

    re-added config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const app = require('./app');
-const port = 5000;
+const port = app.get("port");
 const server = app.listen(port);
 
 server.on('listening', () =>
-  console.log(`Feathers application started on localhost:${port}`)
+  console.log(`Feathers application started on ${app.get("host")}:${port}`)
 );
