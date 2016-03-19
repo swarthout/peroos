4ea4f6f79fb4c1ffe72fce0b5c6ed5be9daa64ce
Scott <swartho9@msu.edu>
2016-03-19 13:55:24 -0400

    summary works

--- src/services/index.js
+++ src/services/index.js
@@ -2,15 +2,10 @@
 const articles = require('./articles');
 const keywords = require('./keywords');
 const summary = require('./summary');
-const authentication = require('./authentication');
-const user = require('./user');
 
 module.exports = function() {
   const app = this;
 
-
-  app.configure(authentication);
-  app.configure(user);
   app.configure(summary);
   app.configure(keywords);
   app.configure(articles);
--- src/services/summary/index.js
+++ src/services/summary/index.js
@@ -1,41 +1,24 @@
 'use strict';
 
 const hooks = require('./hooks');
-const request = require('request');
+const rp = require('request-promise');
 class Service {
   constructor(options = {}) {
     this.options = options;
   }
 
   find(params) {
-  	let url = params.url;
-  	let summary = "";
-  	request({
-  		url: "https://api.import.io/store/connector/a5979ec4-de3c-41c0-9ef6-e8b470e3471f/_query",
-  		qs: {
-  			input: {
-  					url: url
-  					},
-  		_apikey: "5c89801849d746fea1113bef5cb2a4e918bf323ced22c5cb7024ccc9f425a0a5f80851f22f0895279977781bec8211f542bfd2c6c214e98689c78274814f919b4372e092028283ed0916c402a5d6511c"
-  		},
-  		method: "GET"
-  	},function(error,response,body){
-  		if(error){
-  			console.log(error);
-  		}
-  		else{
-  			console.log(body);
-  			summary = body.results[0].summary;
-  		}
-  	})
-  	
-  	Promise.resolve(summary);
+    let req_url = params.query.url;
+    let import_io_url = `https://api.import.io/store/connector/a5979ec4-de3c-41c0-9ef6-e8b470e3471f/_query?input=url:${req_url}&_apikey=5c89801849d746fea1113bef5cb2a4e918bf323ced22c5cb7024ccc9f425a0a5f80851f22f0895279977781bec8211f542bfd2c6c214e98689c78274814f919b4372e092028283ed0916c402a5d6511c`;
+    let summary = "";
+    return rp(import_io_url).then((data) => {
+      let data_obj = JSON.parse(data);
+      return data_obj.results[0].summary;
+    })
   }
 
   get(id, params) {
-    return Promise.resolve({
-      id, text: `A new message with ID: ${id}!`
-    });
+    return Promise.resolve({});
   }
 
   create(data, params) {
