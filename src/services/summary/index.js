03d6619ad18fa8256fc38ac30955a7d848dd8c25
Brody Clarke <brodymclarke@gmail.com>
2016-03-19 20:11:49 +0000

    Adjusted summary

--- src/services/summary/index.js
+++ src/services/summary/index.js
@@ -1,14 +1,35 @@
 'use strict';
 
 const hooks = require('./hooks');
-
+const request = require('request');
 class Service {
   constructor(options = {}) {
     this.options = options;
   }
 
   find(params) {
-    return Promise.resolve([]);
+  	let url = params.url;
+  	let summary = "";
+  	request({
+  		url: "https://api.import.io/store/connector/a5979ec4-de3c-41c0-9ef6-e8b470e3471f/_query",
+  		qs: {
+  			input: {
+  					url: url
+  					},
+  		_apikey: "5c89801849d746fea1113bef5cb2a4e918bf323ced22c5cb7024ccc9f425a0a5f80851f22f0895279977781bec8211f542bfd2c6c214e98689c78274814f919b4372e092028283ed0916c402a5d6511c"
+  		},
+  		method: "GET"
+  	},function(error,response,body){
+  		if(error){
+  			console.log(error);
+  		}
+  		else{
+  			console.log(body);
+  			summary = body.results[0].summary;
+  		}
+  	})
+  	
+  	Promise.resolve(summary);
   }
 
   get(id, params) {
