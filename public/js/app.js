1c3cce1be3f90b1f676fc72caf7580aa8d2329d7
Scott <swartho9@msu.edu>
2016-03-20 10:45:51 -0400

    addded search params

--- public/js/app.js
+++ public/js/app.js
@@ -6,6 +6,30 @@ const app = feathers()
 const summaryService = app.service('/summaries/');
 const videosService = app.service('/relevant_videos/')
 
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+$( document ).ready(function() {
+  var search_url = getUrlParameter('search')
+    if(search_url){
+      console.log(search_url);
+      document.getElementById('query_url').value = search_url;
+      getSummary();
+    }
+});
+
 function getSummary(){
   var url = document.getElementById('query_url').value;
 
